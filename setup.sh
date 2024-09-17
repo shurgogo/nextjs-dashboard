78f3e1e9bc24bb4a54f05d77426a185e7704eb78
shur <shur@Shurs-MacBook-Pro.local>
2024-09-17 12:46:58 +0800

    trouble shoot 9: setup.sh error

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 POSTGRES_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' my-pg)
-sed -i "s/\${POSTGRES_HOST}/$POSTGRES_IP/" .env.prod
+sed -i "s/\${POSTGRES_HOST}/$POSTGRES_IP/" .env.production
 
 
