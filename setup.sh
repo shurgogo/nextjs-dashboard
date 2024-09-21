6d90c5258402cd080f46cc27c0c5c56e9f5713e6
shur <shur@192.168.5.52>
2024-09-21 10:47:27 +0800

    feat.15 add authentication bugfix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 POSTGRES_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' my-pg)
-DASHBOARD_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dash-app)
+# DASHBOARD_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dash-app)
 sed -i "s/\${POSTGRES_HOST}/$POSTGRES_IP/" .env.production
-sed -i "s/\${DASHBOARD_IP}/$DASHBOARD_IP/" .env.production
+# sed -i "s/\${DASHBOARD_IP}/$DASHBOARD_IP/" .env.production
 
 
