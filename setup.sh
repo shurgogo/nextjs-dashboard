a58c1d190623808c1cdbd9b32f68a9619d5d952e
shur <shur@Shurs-MacBook-Pro.local>
2024-09-21 15:15:00 +0800

    issue: fix [auth][warn][env-url-basepath-redundant]

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 POSTGRES_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' my-pg)
-# DASHBOARD_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dash-app)
 sed -i "s/\${POSTGRES_HOST}/$POSTGRES_IP/" .env.production
-# sed -i "s/\${DASHBOARD_IP}/$DASHBOARD_IP/" .env.production
 
 
